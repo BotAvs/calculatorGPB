a82b86317c042c0b915d6b0f8e144e0d7e0903c6
BotAvs <borjasporting12@gmail.com>
2026-05-09 17:50:05 +0000

    Añadido esqueleto Spring Boot

---
+++ calculatorGPB/src/main/java/com/ciberGPB/calculatorGPB/CalculatorGpbApplication.java
@@ -0,0 +1,13 @@
+package com.ciberGPB.calculatorGPB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGpbApplication.class, args);
+	}
+
+}
---
+++ calculatorGPB/src/test/java/com/ciberGPB/calculatorGPB/CalculatorGpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberGPB.calculatorGPB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
